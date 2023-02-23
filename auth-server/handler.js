56ee71cddefbc3ec7cbd150ff1f23b5a4ee0c148
SpenUtt <spencerutt8@gmail.com>
2023-02-23 17:50:07 +0100

    corrected syntax error

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -13,7 +13,7 @@ const credentials = {
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   redirect_uris: ["https://spenutt.github.io/MeetUpp/"],
-  javascript_origins: ["https://spenutt.github.io/MeetUpp", "http://localhost:3000"],
+  javascript_origins: ["https://spenutt.github.io/", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
