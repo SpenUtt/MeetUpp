b0cfc5c0ee6a3b3db45d344c55d07a5c7bc9b0f2
SpenUtt <spencerutt8@gmail.com>
2023-03-06 23:12:22 +0100

    Object-oriented programming, alerts added

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,30 +1,37 @@
 import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
-    state = { num: 32 };
- 
+    state = { num: 32, errorText: "" };
+    
     changeNum = (value) => {
-      this.setState({ num: value });
-      this.props.updateNumberOfEvents(value);
-    }
+        this.setState({ num: value });
+        this.props.updateNumberOfEvents(value);
+        if (value < 1 || value > 32) {
+            this.setState({ errorText: "Select number from 1 to 32" });
+        } else this.setState({ errorText: "" });
+    };
+    
 
     componentDidMount() {
         this.setState({ num: this.props.num || 32 });
     }
  
     render() {
-        const { num } = this.state;
+        const { num, errorText } = this.state;
         return (
-            <label>
-                <input
-                    className="num"
-                    type="number"
-                    value={num}
-                    onChange={(event) => {
-                        this.changeNum(event.target.value);
-                    }}
-                ></input>
-            </label>
+            <div>
+                <ErrorAlert text={errorText} />
+                <label>
+                    Number Of Events
+                    <input
+                        className="num"
+                        type="number"
+                        value={num}
+                        onChange={(event) => this.changeNum(event.target.value)}
+                    ></input>
+                </label> 
+            </div>
         );
     }
 }
