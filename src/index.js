83c8df71167da1066348e794d44bc2a41b2808fe
SpenUtt <spencerutt8@gmail.com>
2023-03-07 14:50:22 +0100

    manifest, serviceWorkerRegistration. Icons added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import * as atatus from 'atatus-spa';
 atatus.config('683d645ec3f345bd9102c40675634c8e').install();
 
@@ -13,7 +15,9 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
